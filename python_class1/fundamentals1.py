d60d9a5509885ee8ab343f575cc363e2aa7315b6
Jon Haase <haase1020@gmail.com>
2020-07-17 12:56:45 -0400

    initial commit with notes

---
+++ python_class1/fundamentals1.py
@@ -0,0 +1,16 @@
+# don't do the following:
+# def foo(a, b=[]):
+
+# instead do:
+#  def foo(a, b=None):
+#     if b is None:
+#         b = []
+
+def calc_nums(x, y, operation="add"):
+    if operation == "add":
+        print(x+y)
+    if operation == "subtract":
+        print(x-y)
+
+
+calc_nums(4, 5, "subtract")
