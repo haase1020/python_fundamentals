32d288f32b2f387a3c7490b68fe15d5bc055b8aa
Jon Haase <haase1020@gmail.com>
2020-07-18 17:50:07 -0400

    notes and files for intermediate python

---
+++ python_class2/car.py
@@ -0,0 +1,13 @@
+class Car:
+    runs = True
+    number_of_wheels = 4
+
+    @classmethod
+    def get_number_of_wheels(cls):
+        return cls.number_of_wheels
+
+    def start(self):
+        if self.runs:
+            print("Car is started. Vroom vroom!")
+        else:
+            print("Car is broken :(")
